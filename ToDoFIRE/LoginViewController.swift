32cacfe3d4e53f513d7330298cda22b2baefcbe7
kneskoromny <kneskoromny@gmail.com>
2021-09-27 08:03:02 +0300

    Initial Commit

---
+++ ToDoFIRE/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ToDoFIRE
+//
+//  Created by Ivan Akulov on 28/02/2017.
+//  Copyright © 2017 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+        
+    }
+
+}
+
---
+++ ToDoFIRE/TasksViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TasksViewController.swift
+//  ToDoFIRE
+//
+//  Created by Ivan Akulov on 28/02/2017.
+//  Copyright © 2017 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+        
+    }
+
+}
